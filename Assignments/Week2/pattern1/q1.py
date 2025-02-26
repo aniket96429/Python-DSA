deb9bdda83d8a8d28ed681f4827dfb8c87a58383
Aniket Thakur <aniketthakur1bab@gmail.com>
2025-02-26 23:32:15 +0530

    Update pattern1 q1

--- Assignments/Week2/pattern1/q1.py
+++ Assignments/Week2/pattern1/q1.py
@@ -12,8 +12,7 @@ Program to print the pattern
 n = int(input("Enter the number: "))
 
 if n == 0:
-    print(0)
-    exit()
+    exit("0")
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
