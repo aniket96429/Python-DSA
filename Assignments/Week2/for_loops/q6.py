8537f29694cc6da2cc9c6f6979cb30217afd228e
Aniket Thakur <aniketthakur1bab@gmail.com>
2025-06-12 13:15:52 +0530

    update q6.py

--- Assignments/Week2/for_loops/q6.py
+++ Assignments/Week2/for_loops/q6.py
@@ -6,13 +6,13 @@ Program to print if a number is a perfct number.
 num = int(input("Enter the number: "))
 
 sum = 0
-for i in range(1, int(num**0.5) + 1):
+for i in range(2, int(num**0.5) + 1):
     if num % i == 0:
         sum += i
         if num // i != i:
             sum += num // i
 
-if (sum - num) == num:
+if (sum + 1) == num and num != 1:
     print("Yes it is a perfect number")
 else:
     print("No it is not a perfect number")
