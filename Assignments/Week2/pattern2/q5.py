f619ea59418550b424b649d1a2f00e5898eab960
Aniket Thakur <aniketthakur1bab@gmail.com>
2025-09-09 22:17:04 +0530

    Updated q5

--- Assignments/Week2/pattern2/q5.py
+++ Assignments/Week2/pattern2/q5.py
@@ -15,8 +15,9 @@ if n == 0:
     exit("0")
 
 for i in range(1, n + 1):
-    if i == 1 or i == n:
-        print(n * "*", end="")
-    else:
-        print("*" + (n - 2) * " " + "*", end="")
+    for j in range(1, n + 1):
+        if i == 1 or i == n or j == 1 or j == n:
+            print("*", end="")
+        else:
+            print(" ", end="")
     print()
